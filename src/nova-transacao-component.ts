e0914983013e9a0d1e92094d81de59cc7a42bc9b
victor3381 <victorgabrielboeri10@gmail.com>
2025-03-05 11:55:52 -0300

    Ajustes 01

--- src/nova-transacao-component.ts
+++ src/nova-transacao-component.ts
@@ -2,7 +2,7 @@ const elementoFormulario = document.querySelector(".block-nova-transacao form") 
 elementoFormulario.addEventListener("submit", function(event) {
     event.preventDefault();
     if (!elementoFormulario.checkValidity()) {
-        alert("Por favor, preencha todos os campos da transação");
+        alert("Por favor, preencha todos os campos da transação!");
         return;
     }
 
@@ -10,26 +10,26 @@ elementoFormulario.addEventListener("submit", function(event) {
     const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement;
     const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement;
 
-    let tipoTransacao: string = inputTipoTransacao.value;
+    let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
     let valor: number = inputValor.valueAsNumber;
     let data: Date = new Date(inputData.value);
 
-    if (tipoTransacao === "Depósito") {
+    if (tipoTransacao == TipoTransacao.DEPOSITO) {
         saldo += valor;
-    } else if (tipoTransacao === "Transferência" || tipoTransacao === "Pagamento de Boleto") {
+    } else if (tipoTransacao == TipoTransacao.TRANSFERENCIA || tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
         saldo -= valor;
     } else {
-        alert("Tipo de transação inválida!");
+        alert("Tipo de Transação é inválido!");
         return;
     }
 
-    elementoSaldo.textContent = saldo.toString();
+    elementoSaldo.textContent = formatarMoeda(saldo);
 
-    const novaTransacao = {
+    const novaTransacao: Transacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data,
-    }
+    };
 
     console.log(novaTransacao);
     elementoFormulario.reset();
